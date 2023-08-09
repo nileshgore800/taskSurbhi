18ba11a975c2b6c4989ffaae47736c8a308d3278
nileshgore800 <58887309+nileshgore800@users.noreply.github.com>
2023-08-09 18:03:06 +0530

    Update MainPage.js
    delete mainpage.js file

--- src/components/mainpages/MainPage.js
+++ src/components/mainpages/MainPage.js
@@ -1,14 +1,1 @@
-import React from 'react'
-import NavbarM from './NavbarM'
-// import SidebarNav from './SidebarNav'
 
-const MainPage = () => {
-  return (
-    <div >
-        <NavbarM />
-        
-    </div>
-  )
-}
-
-export default MainPage
